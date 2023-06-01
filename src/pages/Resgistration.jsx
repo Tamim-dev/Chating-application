fd3207b4956d3c3bd06b3b0f4a0603395e8fbc7d
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2023-06-02 03:00:02 +0600

    2nd

--- src/pages/Resgistration.jsx
+++ src/pages/Resgistration.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Grid, TextField } from "@mui/material";
+import { Grid, TextField, Alert } from "@mui/material";
 import Image from "../components/layout/Image";
 import registrationimg from "../assets/registrationimg.png";
 import HadingText from "../components/layout/HadingText";
@@ -10,6 +10,7 @@ import {
     sendEmailVerification,
 } from "firebase/auth";
 import LoadingButton from "@mui/lab/LoadingButton";
+import { VscEyeClosed, VscEye } from "react-icons/vsc";
 
 let initialValue = {
     email: "",
@@ -17,13 +18,13 @@ let initialValue = {
     password: "",
     error: "",
     Loading: false,
+    eye: false,
 };
 
 const Resgistration = () => {
     let [values, setValues] = useState(initialValue);
     const auth = getAuth();
     let navigate = useNavigate();
-
 
     let handleValues = (e) => {
         setValues({
@@ -35,8 +36,28 @@ const Resgistration = () => {
     let handleClick = () => {
         let { email, fullname, password } = values;
 
-        if(!email){
-            
+        if (!email) {
+            setValues({
+                ...values,
+                error: "Enter your email",
+            });
+            return;
+        }
+
+        if (!fullname) {
+            setValues({
+                ...values,
+                error: "Enter your fullname",
+            });
+            return;
+        }
+
+        if (!password) {
+            setValues({
+                ...values,
+                error: "Enter your password",
+            });
+            return;
         }
 
         setValues({
@@ -77,6 +98,9 @@ const Resgistration = () => {
                             label="Email Address"
                             variant="outlined"
                         />
+                        {values.error.includes("email") && (
+                            <Alert severity="error">{values.error}</Alert>
+                        )}
                     </div>
                     <div className="textfield">
                         <TextField
@@ -87,17 +111,31 @@ const Resgistration = () => {
                             label="Full name"
                             variant="outlined"
                         />
+                        {values.error.includes("name") && (
+                            <Alert severity="error">{values.error}</Alert>
+                        )}
                     </div>
-                    <div className="textfield">
+                    <div className="textfield passtextfield">
                         <TextField
                             onChange={handleValues}
                             name="password"
-                            type="password"
+                            type={values.eye ? "text" :"password"}
                             value={values.password}
                             id="outlined-basic"
                             label="Password"
                             variant="outlined"
                         />
+                        <div
+                            onClick={() =>
+                                setValues({ ...values, eye: !values.eye })
+                            }
+                            className="eye"
+                        >
+                            {values.eye ? <VscEye /> : <VscEyeClosed />}
+                        </div>
+                        {values.error.includes("password") && (
+                            <Alert severity="error">{values.error}</Alert>
+                        )}
                     </div>
                     {values.Loading ? (
                         <LoadingButton
