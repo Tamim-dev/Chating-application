95b3835861197e51bb7c891584a382fd8c00ad43
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2023-06-09 16:40:19 +0600

    pust

---
+++ src/components/layout/Group.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import Image from "./Image";
+import profile from "../../assets/profile.png";
+
+const Group = () => {
+    return (
+        <div className="box">
+            <h3 className="groupheading">Groups List</h3>
+            <div className="list">
+                <div>
+                    <Image className="imgprofile" imgsrc={profile} />
+                </div>
+                <div>
+                    <h3>Friends Reunion</h3>
+                    <p>Hi Guys, Wassup!</p>
+                </div>
+                <div>
+                    <Button size="small" variant="contained">Join</Button>
+                </div>
+            </div>
+            <div className="list">
+                <div>
+                    <Image className="imgprofile" imgsrc={profile} />
+                </div>
+                <div>
+                    <h3>Friends Reunion</h3>
+                    <p>Hi Guys, Wassup!</p>
+                </div>
+                <div>
+                    <Button size="small" variant="contained">Join</Button>
+                </div>
+            </div>
+            <div className="list">
+                <div>
+                    <Image className="imgprofile" imgsrc={profile} />
+                </div>
+                <div>
+                    <h3>Friends Reunion</h3>
+                    <p>Hi Guys, Wassup!</p>
+                </div>
+                <div>
+                    <Button size="small" variant="contained">Join</Button>
+                </div>
+            </div>
+            <div className="list">
+                <div>
+                    <Image className="imgprofile" imgsrc={profile} />
+                </div>
+                <div>
+                    <h3>Friends Reunion</h3>
+                    <p>Hi Guys, Wassup!</p>
+                </div>
+                <div>
+                    <Button size="small" variant="contained">Join</Button>
+                </div>
+            </div>
+            <div className="list">
+                <div>
+                    <Image className="imgprofile" imgsrc={profile} />
+                </div>
+                <div>
+                    <h3>Friends Reunion</h3>
+                    <p>Hi Guys, Wassup!</p>
+                </div>
+                <div>
+                    <Button size="small" variant="contained">Join</Button>
+                </div>
+            </div>
+            <div className="list">
+                <div>
+                    <Image className="imgprofile" imgsrc={profile} />
+                </div>
+                <div>
+                    <h3>Friends Reunion</h3>
+                    <p>Hi Guys, Wassup!</p>
+                </div>
+                <div>
+                    <Button size="small" variant="contained">Join</Button>
+                </div>
+            </div>
+            <div className="list">
+                <div>
+                    <Image className="imgprofile" imgsrc={profile} />
+                </div>
+                <div>
+                    <h3>Friends Reunion</h3>
+                    <p>Hi Guys, Wassup!</p>
+                </div>
+                <div>
+                    <Button size="small" variant="contained">Join</Button>
+                </div>
+            </div>
+            <div className="list">
+                <div>
+                    <Image className="imgprofile" imgsrc={profile} />
+                </div>
+                <div>
+                    <h3>Friends Reunion</h3>
+                    <p>Hi Guys, Wassup!</p>
+                </div>
+                <div>
+                    <Button size="small" variant="contained">Join</Button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Group;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-
-
+import Grid from "@mui/material/Grid";
+import Group from "../components/layout/Group";
+import home from "../design/home.css"
 
 const Home = () => {
-  
-
-  
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <Grid container spacing={2}>
+            <Grid item xs={4}>
+                <Group/>
+            </Grid>
+            <Grid item xs={4}>
+                xs=4
+            </Grid>
+            <Grid item xs={4}>
+                xs=4
+            </Grid>
+        </Grid>
     );
 };
 
