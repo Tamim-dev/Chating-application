7d0bd7691e952998daae814fefd512d3cf1c6f53
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2023-07-15 02:34:45 +0600

    15-7-2023

--- src/components/homeLayout/Friends.jsx
+++ src/components/homeLayout/Friends.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import Button from "@mui/material/Button";
 import Image from "../layout/Image";
 import profile from "../../assets/profile.png";
+import {
+    getDatabase,
+    ref,
+    onValue,
+    set,
+    push,
+    remove,
+} from "firebase/database";
+import { useSelector } from "react-redux";
 
 const Friends = () => {
+    const db = getDatabase();
+    
+    let userData = useSelector((state) => state.loggedUser.loginUser);
+    let [friends, setFriends] = useState([]);
+
+    useEffect(()=>{
+        onValue(ref(db, "friends/"), (snapshot) => {
+            let arr = [];
+            snapshot.forEach((item) => {
+                if(item.val().receiverid == userData.uid || item.val().senderid == userData.uid){                   
+                    arr.push({...item.val(), id: item.key})
+                }
+            });
+            setFriends(arr);
+        });
+    },[])
     return (
         <div className="box">
             <div className="heading">
                 <h3 className="groupheading">Friends</h3>
             </div>
-            <div className="list">
-                <div className="profileImg">
-                    <Image className="imgprofile" imgsrc={profile} />
+            {friends.map(item=>(
+                <div className="list">
+                    <div className="profileImg">
+                        <Image className="imgprofile" imgsrc={profile} />
+                    </div>
+                    <div className="profileName">
+                        {item.receiverid == userData.uid 
+                            ?<h3>{item.sendername}</h3>
+                            :<h3>{item.receivername}</h3>
+                        }
+                        <p>Hi Guys, Wassup!</p>
+                    </div>
+                    <div className="profileBtn">
+                        <p className="time">Today, 8:56pm</p>
+                    </div>
                 </div>
-                <div className="profileName">
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div className="profileBtn">
-                    <p className="time">Today, 8:56pm</p>
-                </div>
-            </div>
+            ))}
         </div>
     );
 };
