ce37d845d9656b231e52ac628109354306be09bb
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2023-07-16 00:11:28 +0600

    16-7-2023

--- src/components/slices/users/userSlice.js
+++ src/components/slices/users/userSlice.js
@@ -1,12 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-
+const initialState = {
+  loginUser: localStorage.getItem("user")? JSON.parse(localStorage.getItem("user")):null
+}
 
 export const counterSlice = createSlice({
   name: 'user',
-  initialState: {
-    loginUser: localStorage.getItem("user")? JSON.parse(localStorage.getItem("user")):null
-  },
+  initialState,
   reducers: {
     userData: (state,actions) => {
       state.loginUser = actions.payload
