a129f67448e4a8f4d85cb2942b11360c692153ad
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2023-07-20 02:30:51 +0600

    20-7-2023

--- src/components/homeLayout/Group.jsx
+++ src/components/homeLayout/Group.jsx
@@ -1,129 +1,200 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import Image from "../layout/Image";
 import profile from "../../assets/profile.png";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import {TextField,Alert} from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { useSelector } from "react-redux";
+import {
+    getDatabase,
+    ref,
+    onValue,
+    set,
+    push,
+    remove,
+} from "firebase/database";
+
+const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 240,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    pt: 3,
+    px: 4,
+    pb: 3,
+};
+
+let initialValue = {
+    groupName: "",
+    groupTagline: "",
+    error:"",
+    loading: false,
+};
 
 const Group = () => {
+    const db = getDatabase();
+    const [open, setOpen] = useState(false);
+    const [groupInfo, setGroupInfo] = useState(initialValue);
+    let [groups, setGroups] = useState([]);
+
+    let userData = useSelector((state) => state.loggedUser.loginUser);
+
+    useEffect(() => {
+        onValue(ref(db, "groups/"), (snapshot) => {
+            let arr = [];
+            snapshot.forEach((item) => {
+                if (userData.uid != item.val().adminId) {
+                    arr.push(item.val());
+                }
+            });
+            setGroups(arr);
+        });
+    }, []);
+
+    let handlechange = (e) => {
+        setGroupInfo({
+            ...groupInfo,
+            [e.target.name]: e.target.value,
+            error:""
+        });
+    };
+
+    let handelClick = () => {
+        let { groupName, groupTagline } = groupInfo;
+        if(!groupName){
+            setGroupInfo({
+                ...groupInfo,
+                error:"GroupName"
+            });
+            return
+        }
+        if(!groupTagline){
+            setGroupInfo({
+                ...groupInfo,
+                error:"GroupTagline"
+            });
+            return
+        }
+        setGroupInfo({
+            ...groupInfo,
+            loading: true,
+        });
+        set(push(ref(db, "groups")), {
+            groupName: groupInfo.groupName,
+            groupTagline: groupInfo.groupTagline,
+            adminId: userData.uid,
+            adminName: userData.displayName,
+        }).then(() => {
+            setGroupInfo({
+                ...groupInfo,
+                groupName: "",
+                groupTagline: "",
+                loading: false,
+            });
+            setOpen(false);
+        });
+    };
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
     return (
         <div className="box">
             <div className="heading">
                 <h3 className="groupheading">Groups List</h3>
-                <Button className="btncolor" size="small" variant="contained">
+                <Button
+                    onClick={handleOpen}
+                    className="btncolor"
+                    size="small"
+                    variant="contained"
+                >
                     Create Group
                 </Button>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="child-modal-title"
+                    aria-describedby="child-modal-description"
+                >
+                    <Box sx={{ ...style, width: 200 }}>
+                        <h2 className="grouppopupname">Create Your Group</h2>
+                        <TextField
+                            value={groupInfo.groupName}
+                            name="groupName"
+                            onChange={handlechange}
+                            margin="dense"
+                            id="outlined-basic"
+                            label="Group Name"
+                            variant="outlined"
+                        />
+                        {groupInfo.error?.includes("GroupName") && (
+                            <Alert className="alerterror" severity="error">
+                                Enter your Group Name
+                            </Alert>
+                        )}
+                        <TextField
+                            value={groupInfo.groupTagline}
+                            name="groupTagline"
+                            onChange={handlechange}
+                            margin="dense"
+                            id="outlined-basic"
+                            label="Group Tagline"
+                            variant="outlined"
+                        />
+                        {groupInfo.error?.includes("GroupTagline") && (
+                            <Alert className="alerterror" severity="error">
+                                Enter your Group Tagline
+                            </Alert>
+                        )}
+                        {groupInfo.loading ? (
+                            <LoadingButton
+                                className="groupbtn"
+                                loading
+                                variant="outlined"
+                            >
+                                Create
+                            </LoadingButton>
+                        ) : (
+                            <Button
+                                className="groupbtn btncolor"
+                                onClick={handelClick}
+                                variant="contained"
+                            >
+                                Create
+                            </Button>
+                        )}
+                    </Box>
+                </Modal>
             </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
+            {groups.map((item) => (
+                <div className="list">
+                    <div className="profileImg">
+                        <Image className="imgprofile" imgsrc={profile} />
+                    </div>
+                    <div className="profileName">
+                        <h3>{item.groupName}</h3>
+                        <p>{item.groupTagline}</p>
+                    </div>
+                    <div className="profileBtn">
+                        <Button
+                            className="btncolor"
+                            size="small"
+                            variant="contained"
+                        >
+                            Join
+                        </Button>
+                    </div>
                 </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <Button className="btncolor" size="small" variant="contained">
-                        Join
-                    </Button>
-                </div>
-            </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
-                </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <Button className="btncolor" size="small" variant="contained">
-                        Join
-                    </Button>
-                </div>
-            </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
-                </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <Button className="btncolor" size="small" variant="contained">
-                        Join
-                    </Button>
-                </div>
-            </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
-                </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <Button className="btncolor" size="small" variant="contained">
-                        Join
-                    </Button>
-                </div>
-            </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
-                </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <Button className="btncolor" size="small" variant="contained">
-                        Join
-                    </Button>
-                </div>
-            </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
-                </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <Button className="btncolor" size="small" variant="contained">
-                        Join
-                    </Button>
-                </div>
-            </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
-                </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <Button className="btncolor" size="small" variant="contained">
-                        Join
-                    </Button>
-                </div>
-            </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
-                </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <Button className="btncolor" size="small" variant="contained">
-                        Join
-                    </Button>
-                </div>
-            </div>
+            ))}
         </div>
     );
 };
--- src/components/homeLayout/Mygroups.jsx
+++ src/components/homeLayout/Mygroups.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import Image from "../layout/Image";
 import profile from "../../assets/profile.png";
+import { useSelector } from "react-redux";
+import {
+    getDatabase,
+    ref,
+    onValue,
+    set,
+    push,
+    remove,
+} from "firebase/database";
 
 const Mygroups = () => {
+    const db = getDatabase();
+    let userData = useSelector((state) => state.loggedUser.loginUser);
+
+    let [groups, setGroups] = useState([]);
+
+    useEffect(() => {
+        onValue(ref(db, "groups/"), (snapshot) => {
+            let arr = [];
+            snapshot.forEach((item) => {
+                if(userData.uid == item.val().adminId){
+                    arr.push(item.val());
+                }
+            });
+            setGroups(arr);
+        });
+    }, []);
+
     return (
         <div className="box">
             <div className="heading">
@@ -12,102 +38,21 @@ const Mygroups = () => {
                     Create Group
                 </Button>
             </div>
-            <div className="list">
-                <div>
+            {groups.map(item=>(
+                <div className="list">
+                <div className="profileImg">
                     <Image className="imgprofile" imgsrc={profile} />
                 </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
+                <div className="profileName">
+                    <h3>{item.groupName}</h3>
+                    <p>{item.groupTagline}</p>
                 </div>
-                <div>
-                <p className="time">Today, 8:56pm</p>
-                </div>
-            </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
-                </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                <p className="time">Today, 8:56pm</p>
+                <div className="profileBtn">
+                    <p className="time">Today, 8:56pm</p>
                 </div>
             </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
-                </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                <p className="time">Today, 8:56pm</p>
-                </div>
-            </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
-                </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                <p className="time">Today, 8:56pm</p>
-                </div>
-            </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
-                </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                <p className="time">Today, 8:56pm</p>
-                </div>
-            </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
-                </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                <p className="time">Today, 8:56pm</p>
-                </div>
-            </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
-                </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                <p className="time">Today, 8:56pm</p>
-                </div>
-            </div>
-            <div className="list">
-                <div>
-                    <Image className="imgprofile" imgsrc={profile} />
-                </div>
-                <div>
-                    <h3>Friends Reunion</h3>
-                    <p>Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                <p className="time">Today, 8:56pm</p>
-                </div>
-            </div>
+            ))}
+            
         </div>
     );
 };
